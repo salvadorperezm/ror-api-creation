674775e554805fff23283ae229fbd10e14e5100d
Salvador Perez <salvadorperezm115@gmail.com>
2022-09-25 10:48:51 -0600

    class 028

--- app/services/posts_search_service.rb
+++ app/services/posts_search_service.rb
@@ -1,5 +1,9 @@
 class PostsSearchService
     def self.search(current_posts, query)
-        current_posts.where("title like '%#{query}%' ")
+        posts_ids = Rails.cache.fetch("posts_search/#{query}", expires_in: 1.hours) do
+            current_posts.where("title like '%#{query}%' ").map(&:id)
+        end
+
+        current_posts.where(id: posts_ids)
     end
 end
