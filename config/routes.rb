ec98f0c9a1ff5e7a0f7b4bb0759132edbb0bcd26
Salvador Perez <salvadorperezm115@gmail.com>
2022-09-24 20:02:57 -0600

    class 012

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/health', to: 'health#health'
+
+  resources :posts, only: [:index, :show]
 end
