07dadbe028a6fb211f3e90348b9bca9429e8657c
Salvador Perez <salvadorperezm115@gmail.com>
2022-09-25 11:07:36 -0600

    class 030

---
+++ app/models/post_report.rb
@@ -0,0 +1,24 @@
+class PostReport < Struct.new(:word_count, :word_histogram)
+    def self.generate(post)
+        PostReport.new(
+            # word count
+            post.content.split.map { |word| word.gsub(/\W/), '' }.count
+
+            # word histogram
+            calc_histogram(post)
+        )
+    end
+
+
+    private 
+    
+    def self.calc_histogram(post)
+        (post
+        .content
+        .split
+        .map { |word| word.gsub(/\W/), '' }
+        .map(&:downcase)
+        .group_by { |word| word }
+        .transform_values(&:size))
+    end
+end
